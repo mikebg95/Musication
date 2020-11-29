720b88bd67f1eb9b7ff6f8687287f93e7ccf5490
Mike <mikebgoldman95@gmail.com>
2020-11-29 03:08:00 +0100

    added bottom navbar with fragments for ChatActivity

--- app/src/main/java/com/example/musication/ChatActivity.java
+++ app/src/main/java/com/example/musication/ChatActivity.java
@@ -1,18 +1,50 @@
 package com.example.musication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private BottomNavigationView navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        navbar = findViewById(R.id.navbar);
+
+        navbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_person:
+                        selectedFragment = new PersonFragment();
+                        break;
+                    case R.id.nav_group:
+                        selectedFragment = new GroupFragment();
+                        break;
+                    case R.id.nav_favorites:
+                        selectedFragment = new FavoriteFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PersonFragment()).commit();
     }
 }
